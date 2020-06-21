3adac159a16f86ca9d19b0cd199e194cd6ab78ad
Siddharth <siddhu.v@gmail.com>
2020-06-21 06:06:52 +0300

    fixed tags-panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EpisodesComponent } from './episodes/episodes.component';
 import { LatestEpisodeComponent } from './latest-episode/latest-episode.component';
 import { TagsPanelComponent } from './tags-panel/tags-panel.component';
+import { FilteredTagsPipe } from './filtered-tags-pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     EpisodesComponent,
     LatestEpisodeComponent,
-    TagsPanelComponent
+    TagsPanelComponent,
+    FilteredTagsPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tags-panel/tags-panel.component.ts
+++ src/app/tags-panel/tags-panel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EpisodeService } from '../episode.service';
 import { FilteredTagsPipe } from '../filtered-tags-pipe';
+
 @Component({
   selector: 'app-tags-panel',
   templateUrl: './tags-panel.component.html',
@@ -9,6 +10,7 @@ import { FilteredTagsPipe } from '../filtered-tags-pipe';
 export class TagsPanelComponent implements OnInit {
 
   tags: {};
+  tagSearch = '';
 
   constructor(private episodeService: EpisodeService) { }
 
